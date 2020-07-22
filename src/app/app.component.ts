8930dd2b8e930005ccae29dd28f6178cfb718459
Yuvraj Patil <yuvraj.patil.it@gmail.com>
2020-07-22 18:32:54 +0530

    Added Github repository logo with correct url. - Increased height of pie chart on mobile devices. - Added command to make build with correct base-ref.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import * as appData from './uv-data.json';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UV Share Market Portfolio';
+  gitLogo = appData.general.gitIcon;
 }
